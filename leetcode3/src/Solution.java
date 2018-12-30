605c8df2de56438b8a5e716e0faa39e3399f82c7
songyunfeng <56113272@qq.com>
2018-12-30 15:09:43 +0800

    额，加了点东西

--- leetcode3/src/Solution.java
+++ leetcode3/src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Auther: songyunfeng
  * @Date: 2018/12/30 14:39
@@ -5,4 +8,18 @@
  */
 public class Solution {
   //试一下//
+    public int lengthOfLogestSubstring(String s ){
+        char[] sc = s.toCharArray();
+        Map<Character, Integer> cm = new HashMap<Character, Integer>();
+        int j = 0 ,maxlen = 0;
+        for (int i = 0 ; i < sc.length ; i++){
+            char cur = sc[i];
+            if (cm.containsKey(cur)){
+                maxlen = Math.max(i-j,maxlen);
+                j = Math.max(j,cm.get(cur)+1);
+            }
+
+        }
+        return Math.max(sc.length - j, maxlen);
+    }
 }
