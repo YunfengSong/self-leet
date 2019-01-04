c8e46fc703578658e86cccc805f72867af4dfc07
songyunfeng <56113272@qq.com>
2019-01-04 19:51:54 +0800

    去杭州，年前应该是最后一次

--- leetcode20/src/Solution.java
+++ leetcode20/src/Solution.java
@@ -1,7 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @Auther: songyunfeng
  * @Date: 2019/1/2 16:59
  * @Description:
  */
 public class Solution {
+    public boolean isValid(String s) {
+        Map<Character,Character> pM = new HashMap<Character, Character>();
+        pM.put('(', ')');
+        pM.put('{', '}');
+        pM.put('[', ']');
+        Stack<Character> pS = new Stack<Character>();
+        for(Character c: s.toCharArray()) {
+            if(pM.containsKey(c)) {
+                pS.push(pM.get(c));
+            } else {
+                if(pS.isEmpty() || c != pS.pop())
+                    return false;
+            }
+        }
+        if(!pS.isEmpty())
+            return false;
+        return true;
+    }
 }
---
+++ leetcode21/src/Solution.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: songyunfeng
+ * @Date: 2019/1/4 10:19
+ * @Description:
+ */
+public class Solution {
+}
