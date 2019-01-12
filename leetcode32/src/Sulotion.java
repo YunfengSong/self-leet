82b3fefb14ecb9adc093a53a41eade21f7923dfa
songyunfeng <56113272@qq.com>
2019-01-12 15:16:34 +0800

    给海霞买了小玩具

--- leetcode32/src/Sulotion.java
+++ leetcode32/src/Sulotion.java
@@ -1,8 +1,26 @@
+import java.util.Stack;
+
 /**
  * @Auther: songyunfeng
  * @Date: 2019/1/10 19:52
  * @Description:
  */
 public class Sulotion {
-    //
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int max = 0;
+        int left = -1;
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '(') stack.push(j);
+            else {
+                if (stack.isEmpty()) left = j;
+                else {
+                    stack.pop();
+                    if (stack.isEmpty()) max = Math.max(max, j - left);
+                    else max = Math.max(max, j - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
 }
