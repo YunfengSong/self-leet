a56c62939e9b05d83cae4e713ad0094817d018be
songyunfeng <56113272@qq.com>
2019-01-13 11:07:18 +0800

    还有11天就可以回家了

--- leetcode31/src/Solution.java
+++ leetcode31/src/Solution.java
@@ -1,3 +1,4 @@
+/*
 public class Solution {
     public void nextPermutation(int[] nums) {
         int i;
@@ -23,4 +24,102 @@ public class Solution {
     private static void reverse(int[] nums, int i, int j) { while (i < j) {
         swap(nums, i++, j--);
     } }
+}*/
+/*public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+
+        int left = 0, right = n - 1;
+        int mid = 0;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
+            if (left == right) {
+                if (target <= nums[left]) {
+                    return left;
+                }
+                return right + 1;
+            }
+
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else if(target == nums[mid])
+                return mid;
+
+
+        }
+
+
+        return -1;
+    }
+}*/
+
+/*
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int left = 0, right = n - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
+            if (target > nums[mid]) {
+                left = Math.min(mid + 1, right);
+            } else if (target < nums[mid]) {
+                right = Math.max(mid - 1, left);
+            } else {
+                return mid;
+            }
+
+            if (left == right) {
+                if (target <= nums[left]) {
+                    return left;
+                }
+                return right + 1;
+            }
+        }
+        return -1;
+    }
+}*/
+public class Solution {
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i++, j--);
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i;
+        for (i = nums.length - 1; i > 0; --i) {
+            if (nums[i] > nums[i - 1]) {
+                break;
+            }
+        }
+        if (i > 0) {
+            for (int j = nums.length - 1; j >= i; --j) {
+                if (nums[j] > nums[i - 1]) {
+                    swap(nums, j, i - 1);
+                    break;
+                }
+            }
+        }
+        reverse(nums, i, nums.length - 1);
+    }
 }
